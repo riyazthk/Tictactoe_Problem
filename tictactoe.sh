fe5dc021434db107d19f0bc55d0f6eae4034ee7b
riyazthk <er.riyaz2507@gmail.com>
2020-07-08 22:41:36 +0530

    [Riyaz]ADDED:To using random function to calculate a toss

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,2 +1,26 @@
 #! /bin/bash -x
 board=(0 0 0 0 0 0)
+
+#constant variables
+HEAD=0
+TAIL=1
+
+function calculateToss(){
+toss=$((RANDOM%2))
+}
+
+calculateToss
+if [[ $toss -eq $HEAD ]]
+then
+   echo "user win a toss"
+   #userVariable=read -p "x or o take anyone"
+else
+   echo "computer win a toss"
+  # tossValue=$(calculateToss)
+   #   if [[ $tossvalue -eq 0 ]]
+    #  then
+     #     computerVariable=o
+      #else
+       #   computerVariable=x
+      #fi
+fi
