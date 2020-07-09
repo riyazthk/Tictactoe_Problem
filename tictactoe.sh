a3b8202f65cccdec1d817d085a88170994d786af
riyazthk <er.riyaz2507@gmail.com>
2020-07-09 15:59:41 +0530

    [Riyaz]ADDED:Add a code for game rule

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-board=(0 0 0 0 0 0 0 0 0)
+board=(_ _ _ _ _ _ _ _ _)
 
 #constant variables
 HEAD=0
@@ -14,36 +14,36 @@ function calculateToss(){
 toss=$((RANDOM%2))
 }
 function computerValue(){
-      local tossvalue=$1
-         if [[ $tossvalue -eq 0 ]]
+      local tossValue=$1
+         if [[ $tossValue -eq 0 ]]
          then
-             computerVariable=3
+             computerVariable="x"
          else
-             computerVariable=4
+             computerVariable="o"
          fi
-             echo $((computerVariable))
+             echo $computerVariable
 }
 
 function userValue(){
-    local computerVariable=$1
-       if [[ $computerVariable -eq 3 ]]
+    local computer=$1
+       if [[ $computer == "x" ]]
        then
-          userVariable=4
+          userVariable="o"
        else
-          userVariable=3
+          userVariable="x"
        fi
-          echo $((userVariable))
+          echo $userVariable
 }
 
 function computerVal(){
     local userVariable=$1
-       if [[ $userVariable -eq 3  ]]
+       if [[ $userVariable == "x" ]]
        then
-           computerVariable=4
+           computerVariable="o"
        else
-           computerVariable=3
+           computerVariable="x"
        fi
-           echo $((computerVariable))
+           echo $computerVariable
 }
 function alternatePlay(){
        local starts=$1
@@ -58,9 +58,9 @@ function alternatePlay(){
 
 function cellChecking(){
      local cell=$1
-        while [[ board[cell]!=0 ]]
+        while [[ board[cell]!="_" ]]
         do
-            if [[ board[cell] -eq 0 ]]
+            if [[ board[cell] -eq "_" ]]
             then
                 break
             else
@@ -69,10 +69,12 @@ function cellChecking(){
         done
                 echo $((cell))
 }
+
 function randomValues(){
-   cell=$((RANDOM%9+1))
+   cell=$((RANDOM%9))
    echo $((cell))
 }
+
 function printValues(){
 
         for i in ${!board[@]}
@@ -82,37 +84,130 @@ function printValues(){
 echo $((0))
 }
 
+function rowChecking(){
+    rowValue=0
+       while [[ $rowValue -lt 9 ]]
+       do
+          firstValue=$(($rowValue))
+          secondValue=$(($firstValue+1))
+          thirdValue=$(($secondValue+1))
+          if [[ ${board[firstValue]} -eq ${board[secondValue]} && ${board[secondValue]} -eq ${board[thirdValue]} ]]
+          then
+             result=1
+             break
+          else
+             result=0
+          fi
+            rowValue=$(($thirdValue + 1))
+       done
+           echo $((result))
+}
+
+function columnChecking(){
+    columnValue=0
+    count=1
+          while [[ $count -le 9 ]]
+          do
+            firstValue=$(($columnValue))
+            secondValue=$(($firstValue+3))
+            thirdValue=$(($secondValue+3))
+            if [[ ${board[firstValue]} -eq ${board[secondValue]} && ${board[secondValue]} -eq ${board[thirdValue]} ]]
+            then
+               count=$(($count+3))
+               result=1
+               break
+            else
+               count=$(($count+3))
+               result=0
+            fi
+               columnValue=$(($thirdValue-5))
+            done
+               echo $((result))
+}
+
+function diagonalChecking(){
+    local count=$1
+           if [[ ${board[0]} -eq ${board[4]} && ${board[4]} -eq ${board[8]} ]]
+           then
+               result=1
+           elif [[ ${board[2]} -eq ${board[4]} && ${board[4]} -eq ${board[6]} ]]
+           then
+               result=1
+           else
+              if [[ $count -eq 8 ]]
+              then
+                  result=2
+              fi
+           fi
+                 echo $((result))
+}
+
+function gameRule(){
+    local count=$1
+       if [[ $count -ge 5 ]]
+       then
+          result=$(rowChecking)
+             if [[ $result -eq 1 ]]
+             then
+                break
+             fi
+           result=$(columnChecking)
+             if [[ $result -eq 1 ]]
+             then
+                break
+             fi
+           result=$(diagonalChecking $count)
+       fi
+          echo $((result))
+}
+
 function playGame(){
      local starts=$1
-     local computerVariables=$2
-     local userVariables=$3
+     local computerSymbols=$2
+     local userSymbols=$3
         while [[ $count -lt ${#board[@]} ]]
         do
            if [[ $starts -eq 1 ]]
            then
               read -p "Enter a cell number" cells
-              board[(cells)]=$(( $userVariable ))
+              board[$cells]=$userSymbols
               count=$(( $count+1 ))
               print=$(printValues)
               starts=$(alternatePlay $starts)
+              result=$(gameRule $count)
+                 if [[ $result -eq 1 ]]
+                 then
+                    break 3
+                 fi
            else
                cell=$(randomValues)
                cell=$(cellChecking $cell)
-               board[(cell)]=$(( $computerVariable ))
+               board[$cell]=$computerSymbols
                count=$(( $count+1 ))
                print=$(printValues)
                starts=$(alternatePlay $starts)
+               result=$(gameRule $count)
+                  if [[ $result -eq 1 ]]
+                  then
+                     break 3
+                  fi
            fi
         done
+              if [[ $result -eq 1 ]]
+              then
+                 echo "match win"
+              else
+                 echo "match tie"
+              fi
 }
 
 calculateToss
 if [[ $toss -eq $HEAD ]]
 then
    echo "user win a toss"
-   read -p "enter a 3 or 4" userVariable
-   computerVariable=$(computerVal $userVariables)
-   $(playGame $userStarts $ComputerVariables $userVariables)
+   read -p "enter a x or o" userVariable
+   computerVariable=$(computerVal $userVariable)
+   $(playGame $userStarts $computerVariable $userVariable)
 else
    echo "computer win a toss"
    tossValue=$(calculateToss)
